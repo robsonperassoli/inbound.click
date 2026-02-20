118da9e3ca41b0bba569e98dead2f5ac1eaa5d5c
Robson Perassoli <robsonperassoli@gmail.com>
2026-02-20 17:40:57 -0300

    manifest and favicon

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -25,6 +25,10 @@ export const Route = createRootRouteWithContext<AppContext>()({
         content: "width=device-width, initial-scale=1",
       },
       {
+        name: "apple-mobile-web-app-capable",
+        content: "Inbound",
+      },
+      {
         title: "Inbound.Click",
       },
     ],
@@ -32,6 +36,30 @@ export const Route = createRootRouteWithContext<AppContext>()({
       {
         rel: "stylesheet",
         href: appCss,
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        href: "/favicon-96x96.png",
+        sizes: "96x96",
+      },
+      {
+        rel: "icon",
+        type: "image/svg+xml",
+        href: "/favicon.svg",
+      },
+      {
+        rel: "shortcut icon",
+        href: "/favicon.ico",
+      },
+      {
+        rel: "apple-touch-icon",
+        sizes: "180x180",
+        href: "/apple-touch-icon.png",
+      },
+      {
+        rel: "manifest",
+        href: "/site.webmanifest",
       },
     ],
   }),
